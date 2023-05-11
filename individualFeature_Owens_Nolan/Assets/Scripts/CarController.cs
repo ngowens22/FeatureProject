fd7830617eee311a662a0d16450d6c8fea63a928
ngowens22 <ngowens@mail.csuchico.edu>
2023-05-10 19:50:59 -0700

    complete

--- individualFeature_Owens_Nolan/Assets/Scripts/CarController.cs
+++ individualFeature_Owens_Nolan/Assets/Scripts/CarController.cs
@@ -11,22 +11,27 @@ public class CarController : MonoBehaviour
     private float verticalInput;
     private float currentSteeringAngle;
     private float currentBrakeForce;
-    private bool isBraking;
+    
+    public bool isBraking;
+    public bool isDrifting;
 
-    [SerializeField] private float motorForce;
-    [SerializeField] private float brakeForce;
+    [Header("Forces")]
+    [SerializeField] public float motorForce;
+    [SerializeField] public float brakeForce;
     [SerializeField] private float maxSteeringAngle;
 
+
+    [Header("Colliders")]
     [SerializeField] private WheelCollider frontLeftWheelCollider;
     [SerializeField] private WheelCollider frontRightWheelCollider;
     [SerializeField] private WheelCollider backLeftWheelCollider;
     [SerializeField] private WheelCollider backRightWheelCollider;
 
+    [Header("Transforms")]
     [SerializeField] private Transform frontLeftWheelTransform;
     [SerializeField] private Transform frontRightWheelTransform;
     [SerializeField] private Transform backLeftWheelTransform;
     [SerializeField] private Transform backRightWheelTransform;
-
 
     private void FixedUpdate()
     {
@@ -34,6 +39,36 @@ public class CarController : MonoBehaviour
         HandleMotor();
         HandleSteering();
         UpdateWheels();
+
+        //drift
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            //forward
+            WheelFrictionCurve ForFrictionCurve = frontLeftWheelCollider.forwardFriction;
+            ForFrictionCurve.stiffness = .5f;
+            backLeftWheelCollider.forwardFriction = ForFrictionCurve;
+            backRightWheelCollider.forwardFriction = ForFrictionCurve;
+
+            //sideways
+            WheelFrictionCurve SidFrictionCurve = frontLeftWheelCollider.sidewaysFriction;
+            SidFrictionCurve.stiffness = .5f;
+            backLeftWheelCollider.sidewaysFriction = SidFrictionCurve;
+            backRightWheelCollider.sidewaysFriction = SidFrictionCurve;
+        }
+        else
+        {
+            //forward
+            WheelFrictionCurve ForFrictionCurve = frontLeftWheelCollider.forwardFriction;
+            ForFrictionCurve.stiffness = 3f;
+            backLeftWheelCollider.forwardFriction = ForFrictionCurve;
+            backRightWheelCollider.forwardFriction = ForFrictionCurve;
+
+            //sideways
+            WheelFrictionCurve SidFrictionCurve = frontLeftWheelCollider.sidewaysFriction;
+            SidFrictionCurve.stiffness = 3f;
+            backLeftWheelCollider.sidewaysFriction = SidFrictionCurve;
+            backRightWheelCollider.sidewaysFriction = SidFrictionCurve;
+        }
     }
 
     private void GetInput()
